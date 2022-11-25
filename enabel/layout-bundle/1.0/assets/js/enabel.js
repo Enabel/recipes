0d5db3a9e8a0f1237700ee0efa91db6e3afe09a2
Damien Lagae <damienlagae@gmail.com>
2022-11-25 11:18:58 +0100

    :sparkles: Layout recipe

---
+++ enabel/layout-bundle/1.0/assets/js/enabel.js
@@ -0,0 +1,3 @@
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/enabel.scss';
+import '@enabel/enabel-bootstrap-theme/dist/js/enabel-bootstrap-theme.min';
