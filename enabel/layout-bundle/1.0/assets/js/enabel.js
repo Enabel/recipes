79e44588ae4e877e1bce3792b339670a68f8851f
Damien Lagae <damienlagae@gmail.com>
2022-11-28 08:14:43 +0100

    :children_crossing: Cleanup

--- enabel/layout-bundle/1.0/assets/js/enabel.js
+++
@@ -1,3 +0,0 @@
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/enabel.scss';
-import '@enabel/enabel-bootstrap-theme/dist/js/enabel-bootstrap-theme.min';
